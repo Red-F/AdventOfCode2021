8544af06853bc8b3c78d9bb296f324102a1ac0b0
Fred Hoogduin <tv@hoogduin.net>
2021-12-10 15:19:06 +0100

    day 1 part 2 was too slow ....

--- src/Days/Day01.cs
+++ src/Days/Day01.cs
@@ -6,14 +6,18 @@ public class Day01 : BaseDay
     public override string PartOne(string input)
     {
         var readings = input.Integers();
-        var rc = readings.Window(2).Count(x => x.Last() > x.First());
+        var rc = readings.Window(2).Select(x => x.ToArray()).Count(x => x[1] > x[0]);
         return rc.ToString();
     }
 
     public override string PartTwo(string input)
     {
         var readings = input.Integers();
-        var rc = readings.Window(3).Select(x => x.Sum()).Window(2).Count(x => x.Last() > x.First());
+        var windows = readings.Window(3).Select(x => x.Sum()).ToArray();
+        var rc = 0L;
+        for (var i = 0; i < windows.Length - 1; i++)
+            if (windows[i + 1] > windows[i])
+                rc++;
         return rc.ToString();
     }
 }
